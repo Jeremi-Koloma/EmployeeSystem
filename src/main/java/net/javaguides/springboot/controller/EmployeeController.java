23be1683d2177774a3621253b5f7daa41a124f62
Jeremi Koloma <kolomajeremi60@gmail.com>
2022-09-21 22:24:38 -0700

    Ajout de l'annotation @CrossOrigin pour la transaction des donnee du backend vers font end

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package net.javaguides.springboot.controller;
 import lombok.AllArgsConstructor;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/")
 @AllArgsConstructor
